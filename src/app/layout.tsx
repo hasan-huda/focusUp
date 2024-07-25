7fb0205cb235167578d253d164875d24ddb3467f
hasan-huda <hasanhuda037@gmail.com>
2024-07-25 15:19:24 -0400

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const bricolageGrotesque = Bricolage_Grotesque({ subsets: ["latin"], variable: '--font-bricolage-grotesque' });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bricolageGrotesque.className}>
+        <Navbar />
+        {children}</body>
     </html>
   );
 }
